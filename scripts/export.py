636a497dc44b864cde3637c4b0b4b95e80d181c7
Akuukis <akuukis@kalvis.lv>
2020-03-26 19:44:14 +0200

    feat: filter bad data, force cumulative amounts

--- scripts/export.py
+++ scripts/export.py
@@ -4,6 +4,7 @@ import os
 import sqlite3
 import json
 import pycountry
+from datetime import date, timedelta
 
 
 def sanitize_record(record_data):
@@ -25,6 +26,74 @@ def sanitize_int(input):
 
     return input
 
+def is_missing_last_datapoint(cases):
+    yesterday = date.today() - timedelta(days=1)
+    for case in cases:
+        if date.fromisoformat(case[0]) == yesterday:
+            return False
+
+    # print(yesterday, [x[0] for x in cases])
+    return True
+
+def is_with_gaps(cases):
+    days = (date.fromisoformat(cases[-1][0]) - date.fromisoformat(cases[0][0])).days
+
+    if days == len(cases) - 1:
+        return False
+    else:
+        # print(days, [x[0] for x in cases])
+        return True
+
+    return days != len(cases) - 1
+
+def force_cumulative_confirmed(cases):
+    error_count = 0
+    curr_value = 0
+    for case in cases:
+        value = int(case[1])
+        if value < curr_value:
+            error_count = error_count + 1
+            case[1] = curr_value
+        else:
+            curr_value = value
+
+    if error_count > 0:
+        return True
+    else:
+        return False
+
+def force_cumulative_deaths(cases):
+    error_count = 0
+    curr_value = 0
+    for case in cases:
+        value = int(case[2])
+        if value < curr_value:
+            error_count = error_count + 1
+            case[2] = curr_value
+        else:
+            curr_value = value
+
+    if error_count > 0:
+        return True
+    else:
+        return False
+
+def force_cumulative_recovered(cases):
+    error_count = 0
+    curr_value = 0
+    for case in cases:
+        value = int(case[3])
+        if value < curr_value:
+            error_count = error_count + 1
+            case[3] = curr_value
+        else:
+            curr_value = value
+
+    if error_count > 0:
+        return True
+    else:
+        return False
+
 def extract_location(country_city):
 
     location = {
@@ -36,6 +105,12 @@ def extract_location(country_city):
     }
 
     country = country_city[0].strip()
+
+    if country == 'France':
+        # Sadly, France is fragmented across 2 entries and even them are missing entries.
+        # To avoid confusion, drop all France subdivisions too.
+        location['status'] = 'error_not_found'
+        return location
 
     if country == 'US':
         country = 'United States'
@@ -90,7 +165,8 @@ def extract_location(country_city):
         'Holy See': 'VAT',
         'Saint Martin': 'MAF',
         'Vatican City': 'VAT',
-
+        'Nigeria': 'NGA',
+        'Niger': 'NER',
     }
 
     if country in hardcoded_countries.keys():
@@ -98,7 +174,8 @@ def extract_location(country_city):
         location['country'] = country_search
 
         if city:
-            print(country_city)
+            # print(country_city)
+            pass
 
         return location
     else:
@@ -121,7 +198,8 @@ def extract_location(country_city):
     # valstij nav iso
     if not location['country']:
         if city:
-            print(country_city)
+            # print(country_city)
+            pass
 
         location['status'] = 'error_not_found'
         return location
@@ -145,8 +223,8 @@ def extract_location(country_city):
 
         return location
 
-    print(country_city)
-    print(pycountry.countries.search_fuzzy(country))
+    # print(country_city)
+    # print(pycountry.countries.search_fuzzy(country))
 
     return location
 
@@ -166,27 +244,44 @@ if __name__ == '__main__':
     countries_arr = []
 
     for country_city in countries_cities:
+        location_data = extract_location(country_city)
+        if location_data['status'] == 'error_not_found':
+            print('[ERROR] Unknown location: ' + debug_details)
+            continue
+        debug_details = 'country="' + country_city[0] + '", subdivision="' + country_city[1] + '"'
+
         c2 = conn.cursor()
         c2.execute('SELECT date, confirmed, deaths, recovered FROM records where country = ? and city = ? order by date asc', country_city)
 
-        cases = c2.fetchall()
-
         countries_records_arr = []
-
-        for case in cases:
+        for case in c2.fetchall():
             countries_records_arr.append([case[0].split(' ')[0], case[1], case[2], case[3]])
+        countries_records_arr = sorted(countries_records_arr, key=lambda case: case[0])
 
-        location_data = extract_location(country_city)
+        if is_missing_last_datapoint(countries_records_arr):
+            print('[ERROR] Missing last datapoint: ' + debug_details)
+            continue
 
-        if location_data['status'] == 'error_not_found':
-            print(country_city)
-        else:
-            countries_arr.append({
-                "country": location_data['country'].name,
-                "county": location_data['county'],
-                "subdivision": location_data['subdivision'],
-                "cases": countries_records_arr
-            })
+        if is_with_gaps(countries_records_arr):
+            print('[ERROR] Dataseries have gap(s): ' + debug_details)
+            continue
+
+        if force_cumulative_confirmed(countries_records_arr):
+            print('[WARN ] Fixed cumulative "confirmed": ' + debug_details)
+
+        if force_cumulative_deaths(countries_records_arr):
+            print('[WARN ] Fixed cumulative "deaths": ' + debug_details)
+
+        if force_cumulative_recovered(countries_records_arr):
+            print('[WARN ] Fixed cumulative "recovered": ' + debug_details)
+
+        countries_arr.append({
+            "country": location_data['country'].name,
+            "county": location_data['county'],
+            "subdivision": location_data['subdivision'],
+            "cases": countries_records_arr
+        })
+        print('[INFO ] Done: ' + debug_details)
 
     with open(filepath_out, 'w') as outfile:
         json.dump(countries_arr, outfile)
